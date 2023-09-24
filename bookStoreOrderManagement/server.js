e689e80c916e2014426d7c871b3476a7c61f4f1c
Pawan Manka <pawanmanka07@gmail.com>
2023-09-24 11:21:34 +0530

    setup env for both services book and order

--- bookStoreOrderManagement/server.js
+++ bookStoreOrderManagement/server.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import 'dotenv/config';
 // Import Routes
-import BookRoutes from './routes/books.routes.js';
+import OrderRoutes from './routes/orders.routes.js';
 const app = express();
 app.get('/', (req, res) => {
   res.send(`Hello World! ${process.env.S3_BUCKET}`);
 });
-app.use('/api/v1/books',BookRoutes);
+app.use('/api/v1/orders',OrderRoutes);
 
 app.listen(3000, () =>
   console.log('Server app listening on port http://localhost:'.process.env.PORT),
