742039155cf87c1650b6fcaac81907a9aada60da
Pawan Manka <pawanmanka07@gmail.com>
2023-09-26 21:14:38 +0530

    workgin on database

--- bookStoreBookManagement/server.js
+++ bookStoreBookManagement/server.js
@@ -2,12 +2,19 @@ import express from 'express';
 import 'dotenv/config';
 // Import Routes
 import BookRoutes from './routes/books.routes.js';
+import {db} from './config/database.config.js';
+
+db.authenticate().then(() => {
+  console.log('Connection has been established successfully.');
+}).catch((error) => {
+  console.error('Unable to connect to the database: ', error);
+});
 const app = express();
 app.get('/', (req, res) => {
-  res.send(`Hello World! ${process.env.S3_BUCKET}`);
+  res.send(`Hello World! ${process.env.PORT}`);
 });
 app.use('/api/v1/books',BookRoutes);
 
-app.listen(3000, () =>
-  console.log('Server app listening on port http://localhost:'.process.env.PORT),
+app.listen(process.env.PORT, () =>
+  console.log('Server app listening on port http://localhost:'+process.env.PORT),
 );
